492c66087ca1e201a324879bfb20a80253120bcb
warley <warleyserafim777@gmail.com>
2022-10-11 10:57:08 -0400

    fix jogos de hoje

--- src/views/index.js
+++ src/views/index.js
@@ -1,17 +1,13 @@
-import React, { useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
-import Badge from 'react-bootstrap/Badge';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import moment from 'moment';
+import React, { useEffect, useState } from 'react';
 import api from '../services/api';
-import ListGroup from 'react-bootstrap/ListGroup';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import './index.css';
+import { Badge, Card, Avatar, Progress, Divider, Timeline, Collapse} from 'antd';
+const { Panel } = Collapse;
+
+
 
 function JogosDia() {
 
-  const [jogos, setJogos] = React.useState([]);
-
+  const [jogos, setJogos] = useState([]);
 
   useEffect(() => {
   var dataHr = new Date();
@@ -54,6 +50,8 @@ function JogosDia() {
   data();
 }, []);
 
+console.log(jogos);
+
   function formatPalpite(palpite) {
     if (palpite === "1") {
       return "Casa vence";
@@ -76,168 +74,98 @@ function JogosDia() {
     }
 
   }
-  function formatPalpiteValor(palpite) {
-    if (palpite > 80) {
-      return (<ProgressBar  variant="success" animated now={palpite} label={`Probabilidade de acerto ${palpite}%`} />)
-      } else if(palpite > 60 && palpite < 80){
-        return (<ProgressBar variant="info" animated now={palpite} label={`Probabilidade de acerto ${palpite}%`} />)
-      } else if(palpite > 40 && palpite < 60){
-        return (<ProgressBar variant="warning" animated now={palpite} label={`Probabilidade de acerto ${palpite}%`} />)
-      } else {
-        return (<ProgressBar variant="danger" animated now={palpite} label={`Probabilidade de acerto ${palpite}%`} />)
-      }
-  }
 
-
-
-  function ScreenreaderLabelExample(porcentagemPalpite) {
-    if (porcentagemPalpite > 80) {
-    return (<ProgressBar variant="success" animated now={porcentagemPalpite} label={`Probabilidade de acerto ${porcentagemPalpite}%`} />)
-    } else if(porcentagemPalpite > 60 && porcentagemPalpite < 80){
-      return (<ProgressBar variant="info" animated now={porcentagemPalpite} label={`Probabilidade de acerto ${porcentagemPalpite}%`} />)
-    } else if(porcentagemPalpite > 40 && porcentagemPalpite < 60){
-      return (<ProgressBar variant="warning" animated now={porcentagemPalpite} label={`Probabilidade de acerto ${porcentagemPalpite}%`} />)
-    } else {
-      return (<ProgressBar variant="danger" animated now={porcentagemPalpite} label={`Probabilidade de acerto ${porcentagemPalpite}%`} />)
-    }
-  }
-
-  function renderItem(jogo) {
-    return <tr>
-      <td colSpan={5}>
-        <div className="text-right"> 
-
-          <h3>Estatisticas</h3>
-        </div> 
-        <ListGroup variant="dark" className='text-right'>
-
-          <div className="row">
-            <div className="col-md-6">
-          <ListGroup.Item>
-            <p><strong>Palpite Vencedor - <Badge bg='primary'>{formatPalpite(jogo.palpite_vencedor)}</Badge>{'\n'}</strong></p>            
-            {ScreenreaderLabelExample(jogo.palpite_vencedor_valor)}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <p><strong>Palpite para Ambos Marcar - <Badge bg={jogo.palpite_ambos === "Yes" ? 'success' : 'danger'}>{formatPalpite(jogo.palpite_ambos)}</Badge></strong></p>
-          </ListGroup.Item>
-          <ListGroup.Item>
-          <p><strong>Acima de 0.5 Gols HT{'\n'}</strong></p>
-          {formatPalpiteValor(jogo.goals05ht)}
-          </ListGroup.Item>
-          <ListGroup.Item>
-          <p><strong>Acima de 1.5 Gols HT{'\n'}</strong></p>
-          {formatPalpiteValor(jogo.goals15ht)}
-          </ListGroup.Item>
-
-        </div>
-        <div className="col-md-6">
-          <ListGroup.Item>
-          <p><strong>Acima de 1.5 Gols FT{'\n'}</strong></p>
-          {formatPalpiteValor(jogo.goals15ft)}
-          </ListGroup.Item>
-          <ListGroup.Item>
-          <p><strong>Acima de 2.5 Gols FT{'\n'}</strong></p>
-          {formatPalpiteValor(jogo.goals25ft)}
-          </ListGroup.Item>
-          <ListGroup.Item>
-          <p><strong>Acima de 3.5 Gols FT{'\n'}</strong></p>
-          {formatPalpiteValor(jogo.goals35ft)}            
-          </ListGroup.Item>
-        </div>
-        </div>
-        </ListGroup>
-      </td>
-    </tr>
-  }
-
-  function toggleExpand(jogo) {
-    setJogos(jogos.map(function (e) {
-      if (e === jogo) {
-        e.open = !e.open;
-      }
-      return e;
-    }));
+  const onChange = (value) => {
+    console.log('changed', value);
   }
 
   return (
-
-  
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h1 className="text-center">Jogos</h1>
-
-            <Table striped bordered hover variant="dark" className="text-center"
-              responsive="md"
-
-                style={{
-                  width: '100%',
-                  overflowX: 'auto',
+    <>
+    
+      <div className="site-card-wrapper">
+        <div className="row"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          {jogos.map((jogo) => (
+            <div
+              style={
+                {
                   display: 'inline-block',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '80%',
+                  height: 'auto',
+                  marginBottom: '10px',
+                }
+              }
+            >
+              <Badge.Ribbon text={jogo.league_name} color="red">
+                <Card title={jogo.home_team_name + " x " + jogo.away_team_name}>
+                  <div className="row"
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      width: '100%',
+                      height: '100%',
+                      marginBottom: '10px',
+                    }}
+                  >
+                  <Avatar shape='square' src={jogo.logo_home_team} size={70}
+                    style={{
+                      marginRight: '10px',
+                    }}
+                  />
+                  <Avatar shape='square' src={jogo.logo_away_team} size={70}
+                    style={{
+                      marginLeft: '10px',
+                    }}
+                  />
+      
+                  </div>
+                  <Divider orientation="left">Palpites</Divider>
+                  <Collapse onChange={onChange}>
+                    <Panel header="Palpites Jogo" key="1">
+                  <Timeline>
+                    <Timeline.Item color="green">Vencedor: {formatPalpite(jogo.palpite_vencedor)} - 
+                    <Progress percent={jogo.palpite_vencedor_valor} size="small" status="active" />
+                    </Timeline.Item>
+                    <Timeline.Item color="green">Ambos Marcam: {formatPalpite(jogo.palpite_ambos)}
+                    <Progress percent={jogo.palpite_ambos_valor} size="small" status="active" />
+                    </Timeline.Item>
+                    <Timeline.Item color="green">Gols 0-5 HT:
+                    <Progress percent={jogo.goals05ht} size="small" status="active" />
+                    </Timeline.Item>
+                    <Timeline.Item color="green">Gols 1-5 HT:
+                    <Progress percent={jogo.goals15ht} size="small" status="active" />
+                    </Timeline.Item>
+                    <Timeline.Item color="green">Gols 1-5 FT:
+                    <Progress percent={jogo.goals15ft} size="small" status="active" />
+                    </Timeline.Item>
+                    <Timeline.Item color="green">Gols 2-5 FT:
+                    <Progress percent={jogo.goals25ft} size="small" status="active" />
+                    </Timeline.Item>
+                    <Timeline.Item color="green">Gols 3-5 FT:
+                    <Progress percent={jogo.goals35ft} size="small" status="active" />
+                    </Timeline.Item>
+                  </Timeline>
+                  </Panel>
+                  </Collapse>
+                  
 
 
-
-                 }}
-              >
-            
-              <thead>
-                <tr>
-                  <th>Liga</th>
-                  <th>Jogo</th>
-                  <th>Horario</th>
-                  <th>Palpite</th>
-                  <th>0.5 HT</th>
-                  <th>1.5 HT</th>
-                  <th>1.5 FT</th>
-                  <th>2.5 FT</th>
-                  <th>3.5 FT</th>
-                  <th>Palpite Ambos</th>
-                </tr>
-              </thead>
-                {jogos.map(jogo => {
-                  return <tbody><tr key={jogo.home_team_name} className="collapsed" >              
-                    <td>{jogo.league_name}</td>
-                    <td>
-                      <div onClick={() => toggleExpand(jogo)}>
-                      <img src={jogo.logo_home_team} alt="logo" width="30" height="30" /> {' '}
-                      <Badge bg='primary'>{jogo.home_team_name}</Badge>
-                      {" vs "}
-                      <Badge bg='primary'>{jogo.away_team_name}</Badge> {' '}
-                      <img src={jogo.logo_away_team} alt="logo" width="30" height="30" />
-                      </div> 
-                    </td>
-                    <td>{moment(jogo.starting_at).format("DD/MM/YYYY - hh:mm:ss A")}</td>
-                    <td>{formatPalpite(jogo.palpite_vencedor)} - {jogo.palpite_vencedor_valor}%</td>
-                    <td>
-                      <Badge bg='primary'>{jogo.goals05ht} %</Badge>
-                    </td>
-                    <td>
-                      <Badge bg='primary'>{jogo.goals15ht} %</Badge>
-                    </td>
-                    <td>
-                      <Badge bg='primary'>{jogo.goals15ft} %</Badge>
-                    </td>
-                    <td>
-                      <Badge bg='primary'>{jogo.goals25ft} %</Badge>
-                    </td>
-                    <td>
-                      <Badge bg='primary'>{jogo.goals35ft} %</Badge>
-                    </td>
-                    <td>{formatPalpite(jogo.palpite_ambos)} - {jogo.palpite_ambos_valor}%</td>
-                  </tr>
-
-                  {jogo.open ? renderItem(jogo) : null}
-                  </tbody>
-                })}
-            </Table>
-          </div>
-          </div>
-          
+                </Card>
+              </Badge.Ribbon>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
 
+    </>
   );
+ 
 }
 
 export default JogosDia;
