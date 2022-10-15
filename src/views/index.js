c7d1f624e0ac3298c472ea850975202ecbe22f1f
warley <warleyserafim777@gmail.com>
2022-10-15 17:15:54 -0400

    feature estaticas

--- src/views/index.js
+++ src/views/index.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSoccerBall } from '@fortawesome/free-solid-svg-icons'
+
 import api from '../services/api';
-import { Badge, Card, Avatar, Progress, Divider, Timeline, Collapse} from 'antd';
+import { Badge, Card, Avatar, Progress, Divider, Timeline, Collapse, Skeleton, Spin, Descriptions} from 'antd';
 const { Panel } = Collapse;
+
+
+
+
 
 
 
 function JogosDia() {
 
   const [jogos, setJogos] = useState([]);
+  const [teams , setTeams] = useState([]);
+  const [loadingStats , setLoadingStats] = useState(true);
+  const [loadingTeams , setLoadingTeams] = useState(true);
+
+
 
   useEffect(() => {
   var dataHr = new Date();
@@ -17,6 +29,8 @@ function JogosDia() {
     const response = await api.get('/eventApi/'+ dataHrFormatada2 + '/-240/web_mfdz408n8lkuevmq');
     var data = response.data;
     var jogos = data.data;
+
+
 
     var jogosArray = [];
 
@@ -38,17 +52,28 @@ function JogosDia() {
         jogo.goals15ft = jogos[i].goals15ft
         jogo.goals25ft = jogos[i].goals25ft
         jogo.goals35ft = jogos[i].goals35ft
-
+        jogo.localteam_id = jogos[i].localteam_id
+        jogo.visitorteam_id = jogos[i].visitorteam_id
+        
 
         jogosArray.push(jogo);
 
       }
     }
-    setJogos(jogosArray);
+
+    if (jogosArray.length > 0) {
+      setJogos(jogosArray);
+      setLoadingTeams(false);
+    }
 
   }
   data();
 }, []);
+
+  async function getDadosTeams(id){
+    const response = await api.get(`/getTeamStatsDirect/${id}`);
+    return response.data;
+  }
 
   function formatPalpite(palpite) {
     if (palpite === "1") {
@@ -77,11 +102,86 @@ function JogosDia() {
 
   }
 
-  const onChange = (value) => {
+
+  function renderStats() {
+    if (loadingStats) {
+      return (
+        <Skeleton active />
+      )
+    }
+
+    if (teams) {
+    return teams.map((team) => {
+      return (
+        <div className='row'>
+          <div className='col-12'>
+            <Card>
+              <div className='row'>
+                <div className='col-12'>
+                  {team.stats.data.map((stat) => {
+                    return (
+                      <div className='row'>
+                        <div className='col-12'>
+                          <Descriptions title={team.name} layout="vertical" bordered>
+                            <Descriptions.Item label={`Ultimos: ${stat.win.total + stat.lost.total} Jogos`}>
+                              <Badge className='badgeIcons' status="success" text={`Vitórias: ${stat.win.total}`} /> {<br/>}
+                              <Badge className='badgeIcons' status="error" text={`Derrotas: ${stat.lost.total}`} />                
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Media Escanteios">{stat.avg_corners} {' '}
+                              <FontAwesomeIcon icon={faSoccerBall} />
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Media Gols Sofridos por Jogo">
+                              <Badge className='badgeIcons' status="error" text={`Sofridos: ${stat.avg_goals_per_game_conceded.total}`} />
+                              <FontAwesomeIcon icon={faSoccerBall} /> {<br/>}
+                              <Badge className='badgeIcons' status="success" text={`Marcados: ${stat.avg_goals_per_game_scored.total}`} />
+                              <FontAwesomeIcon icon={faSoccerBall} />
+                            </Descriptions.Item>
+                          </Descriptions>
+                                                                        
+                        </div>
+                      </div>
+                    )
+
+                  }
+                  )}
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>    
+      )
+    })
+  }
+  }
+  const onChange = async (value) => {
+    if (value) {
+      try {
+      setLoadingStats(true);
+      var teams = [];
+      setTeams(teams);
+      teams.push(await getDadosTeams(value[0]));
+      teams.push(await getDadosTeams(value[1]));
+      setTeams(teams);
+      console.log({ teams})
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoadingStats(false);
+      }
+    }
     console.log('changed', value);
   }
 
+  if (loadingTeams) {
+    return (
+      <div  style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <Spin/>
+      </div>
+    )
+  } else {
+
   return (
+    
     <>
     
       <div className="site-card-wrapper">
@@ -128,36 +228,42 @@ function JogosDia() {
       
                   </div>
                   <Divider orientation="left">Palpites</Divider>
-                  <Collapse onChange={onChange}>
+                  <Collapse>
                     <Panel header="Palpites Jogo" key="1">
                   <Timeline>
-                    <Timeline.Item color="green">Vencedor: {formatPalpite(jogo.palpite_vencedor)}
+                    <Timeline.Item color="green"><strong>Vencedor: {formatPalpite(jogo.palpite_vencedor)}</strong>
                     <Progress percent={jogo.palpite_vencedor_valor} size="small" status="active" />
                     </Timeline.Item>
-                    <Timeline.Item color="green">Ambos Marcam: {formatPalpite(jogo.palpite_ambos)}
+                    <Timeline.Item color="green"><strong>Ambos Marcam: {formatPalpite(jogo.palpite_ambos)}</strong>
                     <Progress percent={jogo.palpite_ambos_valor} size="small" status="active" />
                     </Timeline.Item>
-                    <Timeline.Item color="green">Gols 0-5 HT:
+                    <Timeline.Item color="green"><strong>Gols 0.5 HT:</strong>
                     <Progress percent={jogo.goals05ht} size="small" status="active" />
                     </Timeline.Item>
-                    <Timeline.Item color="green">Gols 1-5 HT:
+                    <Timeline.Item color="green"><strong>Gols 1.5 HT:</strong>
                     <Progress percent={jogo.goals15ht} size="small" status="active" />
                     </Timeline.Item>
-                    <Timeline.Item color="green">Gols 1-5 FT:
+                    <Timeline.Item color="green"><strong>Gols 1.5 FT:</strong>
                     <Progress percent={jogo.goals15ft} size="small" status="active" />
                     </Timeline.Item>
-                    <Timeline.Item color="green">Gols 2-5 FT:
+                    <Timeline.Item color="green"><strong>Gols 2.5 FT:</strong>
                     <Progress percent={jogo.goals25ft} size="small" status="active" />
                     </Timeline.Item>
-                    <Timeline.Item color="green">Gols 3-5 FT:
+                    <Timeline.Item color="green"><strong>Gols 3.5 FT:</strong>
                     <Progress percent={jogo.goals35ft} size="small" status="active" />
                     </Timeline.Item>
                   </Timeline>
                   </Panel>
                   </Collapse>
-                  
+
+                  <Divider orientation="left">Estatísticas</Divider>
 
 
+                  <Collapse onChange={e => onChange([jogo.localteam_id, jogo.visitorteam_id])}>
+                    <Panel header="Estatistica Times Jogo" key="1">
+                      {renderStats()}
+                    </Panel>
+                  </Collapse>
                 </Card>
               </Badge.Ribbon>
             </div>
@@ -169,9 +275,9 @@ function JogosDia() {
   );
  
 }
+}
 
 export default JogosDia;
-
 
 
 
