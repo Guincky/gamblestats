d6853812f974e89a37525fb18ae6b0479270b587
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-27 14:07:33 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -1,1 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSoccerBall } from '@fortawesome/free-solid-svg-icons'
+
+import api from '../services/api';
+import { Badge, Card, Avatar, Progress, Divider, Timeline, Collapse, Skeleton, Spin, Descriptions} from 'antd';
+const { Panel } = Collapse;
 
