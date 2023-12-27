d6d948efcc0a3677f34cc6ae1301d49d6586af23
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-27 14:43:04 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -6,4 +6,10 @@ import api from '../services/api';
 import { Badge, Card, Avatar, Progress, Divider, Timeline, Collapse, Skeleton, Spin, Descriptions} from 'antd';
 const { Panel } = Collapse;
 
-<center><h1>rerwerwer</h1></center>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
