bb72bf4414f15ef99d176259fd0f496a613fbb01
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 00:15:17 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -7,11 +7,11 @@ const Entrada = () => {
     <div className="container">
       <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
       <div className="botoes-container">
-        <a href="/pagina1">
-          <button className="botao">Ir para Página 1</button>
+        <a href="/jogosdehoje">
+        <button className="botao">Jogos de hoje</button>
         </a>
-        <a href="/pagina2">
-          <button className="botao">Ir para Página 2</button>
+        <a href="/jogosdeamanha">
+          <button className="botao">Jogos de amanhã</button>
         </a>
       </div>
     </div>
