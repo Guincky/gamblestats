9517293a9a4e470f4a31b643bded849a263453eb
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 13:29:16 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../views/entrada.css'; // Importe seu arquivo de estilo
-import BolasFlutuantes from '../views/BolasFlutuante.js';
 
 const Entrada = () => {
   
   return (
     <div className="container">
-    <BolasFlutuantes />
       <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
     <p className="descricao">Análises geradas por inteligência artificial para lhe fornecer informações privilegiadas. Lembre-se que são estastísticas e não palpites!</p>
       <div className="botoes-container">
