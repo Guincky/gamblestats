85bd977ecfbe0acd50aac2df2570c0f7290a3c7d
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 01:43:26 -0300

    Update jogosAmanha.js

--- src/views/jogosAmanha.js
+++ src/views/jogosAmanha.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import React, { useState } from 'react';
 import Popup from '../components/Popup';
 import Table from 'react-bootstrap/Table';
 import Badge from 'react-bootstrap/Badge';
