140e090d5af711b4a327ccab38f17b04cca8b828
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 01:08:38 -0300

    Create Popup.js

---
+++ src/views/Popup.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './Popup.css';
+
+const Popup = ({ message, onClose }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    onClose();
+  };
+
+  return isOpen ? (
+    <div className="popup">
+      <div className="popup-content">
+        <span className="close-btn" onClick={handleClose}>&times;</span>
+        <p>{message}</p>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Popup;
