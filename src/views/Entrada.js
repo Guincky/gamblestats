a1c6471fb3c9ddb34b2c22159192a39dbfb3ad99
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 13:57:49 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -5,7 +5,13 @@ const Entrada = () => {
   
   return (
     <div className="container">
-      <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
+          <div className="bolas-flutuantes">
+        <div className="bola bola-1"></div>
+        <div className="bola bola-2"></div>
+        <div className="bola bola-3"></div>
+        <div className="bola bola-4"></div>
+        {/* Adicione mais bolas conforme necessário */}
+    <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
     <p className="descricao">Análises geradas por inteligência artificial para lhe fornecer informações privilegiadas. Lembre-se que são estastísticas e não palpites!</p>
       <div className="botoes-container">
         <a href="/hoje">
