7b4e7b5740b94c33d5fe6b25ec3f31eab970237a
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 13:20:04 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -6,7 +6,7 @@ const Entrada = () => {
   return (
     <div className="container">
       <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
-    <p className="descricao">Análises geradas por inteligência artifical para lhe fornecer informações privilegiadas. Lembre-se que são estastísticas e não palpites!</p>
+    <p className="descricao">Análises geradas por inteligência artificial para lhe fornecer informações privilegiadas. Lembre-se que são estastísticas e não palpites!</p>
       <div className="botoes-container">
         <a href="/hoje">
         <button className="botao">Jogos de hoje</button>
