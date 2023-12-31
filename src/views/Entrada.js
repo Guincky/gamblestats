bf7a055ae587f1c3e99f0360e27010ba37230c99
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 13:32:13 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -14,6 +14,11 @@ const Entrada = () => {
         <a href="/amanha">
           <button className="botao">Jogos de amanhã</button>
         </a>
+    <div className="bolas-flutuantes">
+        <div className="bola"></div>
+        <div className="bola"></div>
+        {/* Adicione mais bolas conforme necessário */}
+      </div>
       </div>
     </div>
   );
