999c6df4fe5cc0c0c267fa4e5d66fd60294395b1
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 13:23:32 -0300

    Create BolasFlutuantes.js

---
+++ src/components/BolasFlutuantes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BolasFlutuantes.css';
+
+const BolasFlutuantes = () => {
+  return (
+    <div className="bolas-flutuantes">
+      <div className="bola"></div>
+      <div className="bola"></div>
+      {/* Adicione mais bolas conforme necessário */}
+    </div>
+  );
+};
+
+export default BolasFlutuantes;
