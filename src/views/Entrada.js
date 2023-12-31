56388c6d2e2a83b7e2f91642f368c272b6974493
Guincky <77854048+Guincky@users.noreply.github.com>
2023-12-31 00:06:35 -0300

    Update Entrada.js

--- src/views/Entrada.js
+++ src/views/Entrada.js
@@ -5,7 +5,7 @@ const Entrada = () => {
   
   return (
     <div className="container">
-      <h1 className="titulo-bem-vindo">Bem-vindo ao Meu Site!</h1>
+      <h1 className="titulo-bem-vindo">Seja bem-vindos! Escolha se deseja ver os jogos de hoje ou de amanhã!</h1>
       {/* Outros componentes e conteúdo podem vir aqui */}
     </div>
   );
