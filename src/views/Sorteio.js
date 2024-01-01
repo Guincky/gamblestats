a54bb18c4cf686026e037a2cc452bb3e3a2eec12
Guincky <77854048+Guincky@users.noreply.github.com>
2024-01-01 16:37:32 -0300

    Update Sorteio.js

--- src/views/Sorteio.js
+++ src/views/Sorteio.js
@@ -6,7 +6,7 @@ const Sorteio = () => {
   return (
     <div className="container">
     <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
-    <p className="descricao">Análises geradas por inteligência artificial para lhe fornecer informações privilegiadas. Lembre-se que são estastísticas e não palpites!</p>
+    <p className="descricao">Estamos sorteando um prêmio total de 5 mil reais para ao todo 3 pessoas. Para participar basta clicar no botão abaixo e preencher os campos e realizar os pedidos. Boa sorte! </p>
       <div className="botoes-container">
         <a href="/hoje">
         <button className="botao">Participe</button>
