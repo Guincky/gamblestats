77f616f82bb8daf8ca15251a80cdab65a4aa132e
Guincky <77854048+Guincky@users.noreply.github.com>
2024-01-01 14:19:26 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import JogosDia from "../views";
 import Entrada from "../views/Entrada.js";
-// import Header from "../components/header";
 import JogosAmanha from "../views/jogosAmanha";
 
 const Routes = () => {
