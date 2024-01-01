4dda2b0a5256b6081d5132bf0e7459c115689e00
Guincky <77854048+Guincky@users.noreply.github.com>
2024-01-01 16:40:10 -0300

    Update Sorteio.js

--- src/views/Sorteio.js
+++ src/views/Sorteio.js
@@ -2,15 +2,19 @@ import React from 'react';
 import '../views/entrada.css'; // Importe seu arquivo de estilo
 
 const Sorteio = () => {
-  
+  const redirecionarParaOutroSite = () => {
+    // Substitua 'https://www.seusiteexterno.com' pelo URL do site externo para o qual deseja redirecionar
+    window.location.href = 'https://www.seusiteexterno.com';
+  };
+
   return (
     <div className="container">
-    <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
-    <p className="descricao">Estamos sorteando um prêmio total de 5 mil reais para ao todo 3 pessoas. Para participar basta clicar no botão abaixo e preencher os campos e realizar os pedidos. Boa sorte! </p>
+      <h1 className="titulo-bem-vindo">GAMBLESTATS</h1>
+      <p className="descricao">Estamos sorteando um prêmio total de 5 mil reais para ao todo 3 pessoas. Para participar basta clicar no botão abaixo e preencher os campos e realizar os pedidos. Boa sorte! </p>
       <div className="botoes-container">
-        <a href="/hoje">
-        <button className="botao">Participe</button>
-        </a>
+        <button className="botao" onClick={redirecionarParaOutroSite}>
+          Participe
+        </button>
       </div>
     </div>
   );
