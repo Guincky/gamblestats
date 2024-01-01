03289641c03a4973f8bccf77fe8f0fe2e5d2d888
Guincky <77854048+Guincky@users.noreply.github.com>
2024-01-01 16:13:25 -0300

    Update index.js

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSoccerBall } from '@fortawesome/free-solid-svg-icons'
+import { Modal, Button } from 'antd';
+
 
 import api from '../services/api';
 import { Badge, Card, Avatar, Progress, Divider, Timeline, Collapse, Skeleton, Spin, Descriptions} from 'antd';
